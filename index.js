28dc79fc19078d466237e26204ce39d285811e6d
khs3737 <zx7589@gmail.com>
2022-07-21 03:03:25 +0900

    v0.1.3

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 const defaultIdleImageUrl = chrome.runtime.getURL('/images/cat_idle_1.png');
 const defaultClickedImageUrl = chrome.runtime.getURL('/images/cat_clicked_1.png')
 
-const showClickedImage = () => {
-    chrome.storage.sync.get('clickedImage', (result) => {
-        console.log({result});
-        cat.setAttribute('src',result['clickedImage'] ?? defaultClickedImageUrl);
-    });}
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-var cat = document.createElement('img');
+let isClicked = false;
+
+const onClickCat = async () => {
+    if(!isClicked){
+        cat.src = defaultClickedImageUrl;
+        isClicked = true;
+        sleep(1500).then(() => {
+            cat.src = defaultIdleImageUrl;
+            isClicked = false;
+        });
+    }  
+};
+
+const onDragCat = (event) => {
+    let shiftX = event.clientX - cat.getBoundingClientRect().right;
+    let shiftY = event.clientY - cat.getBoundingClientRect().bottom;
+    cat.style.position = 'absolute';
+    cat.style.right = window.innerWidth - event.pageX + shiftX + 'px';
+    cat.style.bottom = window.innerHeight - event.pageY + shiftY + 'px';
+    const onMouseMove = (event) => {
+        cat.style.position = 'absolute';
+        cat.style.right = window.innerWidth - event.pageX + shiftX + 'px';
+        cat.style.bottom = window.innerHeight - event.pageY + shiftY + 'px';
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    cat.onmouseup = (event) => {
+        document.removeEventListener('mousemove', onMouseMove);
+        cat.style.position = 'fixed';
+        cat.style.bottom = '0px';
+        cat.style.right = window.innerWidth - event.pageX;
+    }
+};
+
+let cat = document.createElement('img');
+
 cat.style.position = 'fixed';
 cat.style.bottom = '0px';
 cat.style.right = '100px';
-cat.setAttribute('alt','cat');
-cat.onclick = showClickedImage;
-cat.ondrag
+cat.style.zIndex = '10000';
+cat.src = defaultIdleImageUrl;
+cat.alt = 'cat';
 
-chrome.storage.sync.get('idleImage', (result) => {
-    console.log({result});
-    cat.setAttribute('src',result['idleImage'] ?? defaultIdleImageUrl);
-});
+cat.onclick = onClickCat;
+
+cat.draggable = true;
+cat.onmousedown = onDragCat;
+cat.ondragstart = () => false;
 
 document.body.appendChild(cat);
